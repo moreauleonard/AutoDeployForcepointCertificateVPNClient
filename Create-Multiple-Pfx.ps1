f30035c6d4b43523d0bf4d1d9666cc1dcdb6e3ce
Léonard Moreau <leonard.moreau@epfedu.fr>
2020-11-19 10:36:47 +0100

    Removed 'echo' to user powershell Out-File

--- Create-Multiple-Pfx.ps1
+++ Create-Multiple-Pfx.ps1
@@ -5,7 +5,7 @@ $USERS = (Get-Content $USER_LIST_FILE).split()
 $OUTPUT_USER_FILE = "$BASE_PATH_FS\pfx-pass_users.txt"
 $PASSWORD_LENGTH = 20 # TODO, check that this is an integer
 
-echo '' > $OUTPUT_USER_FILE # create or empty file
+'' | Out-File -FilePath $OUTPUT_USER_FILE # create or empty file
 
 foreach ($user in $USERS) {
 	# TODO : check that directories exists, or create it (_PRIVKEY, _CRT, _PFX)
@@ -13,6 +13,6 @@ foreach ($user in $USERS) {
 	$crt = "$BASE_PATH\_CRT\$user.crt"
 	$pfx = "$BASE_PATH\_PFX\$user.pfx"
 	$pfx_pass = openssl rand -hex ($PASSWORD_LENGTH / 2) # 1 hex = 2 char
-	echo "$user,$pfx_pass" >> $OUTPUT_USER_FILE # we store pfx password somewhere, need it to import it later
+	"$user,$pfx_pass`n" | Out-File -FilePath $OUTPUT_USER_FILE -Append # we store pfx password somewhere, need it to import it later
 	openssl pkcs12 -export -inkey $key -in $crt -passout pass:$pfx_pass -out $pfx
 }
